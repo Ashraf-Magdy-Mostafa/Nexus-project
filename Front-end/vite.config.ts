f18af7e1f5902c384a074a4fc64cea43066e91db
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-09-27 22:34:11 +0300

    deployment fix

--- Front-end/vite.config.ts
+++ Front-end/vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,5 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
+  },
+  preview: {
+    port: parseInt(process.env.PORT || "5173"),
+    host: "0.0.0.0",
+    allowedHosts: [
+      "motivated-spirit-production-5e5e.up.railway.app"
+    ]
   }
 })
