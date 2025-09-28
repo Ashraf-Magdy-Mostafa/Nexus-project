c4a2c7e1787f8fb8af8b318b3a400e899827ec60
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-09-28 22:53:02 +0300

    s

--- Back-end/config/urls.py
+++ Back-end/config/urls.py
@@ -1,12 +1,17 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from shop.views import RegisterView, health
+from django.shortcuts import redirect
+
+
+def home(_):
+    return redirect("/api/schema/swagger/")
+
 
 urlpatterns = [
-
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema")),
